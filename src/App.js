a789b0feca0990c154d55ce86755bb8f083b25ea
Simon Wan <simonwan@Simons-MacBook-Pro-2.local>
2023-06-13 15:22:00 -0400

    Deployed

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
-import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import ReactGA from "react-ga4";
-
-import Homepage from "./pages/homepage";
-import About from "./pages/about";
-import Projects from "./pages/projects";
-import Articles from "./pages/articles";
-import ReadArticle from "./pages/readArticle";
-import Contact from "./pages/contact";
-import Notfound from "./pages/404";
-
-import { TRACKING_ID } from "./data/tracking";
-import "./app.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	useEffect(() => {
-		if (TRACKING_ID !== "") {
-			ReactGA.initialize(TRACKING_ID);
-		}
-	}, []);
-
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/" element={<Homepage />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/projects" element={<Projects />} />
-				<Route path="/articles" element={<Articles />} />
-				<Route path="/article/:slug" element={<ReadArticle />} />
-				<Route path="/contact" element={<Contact />} />
-				<Route path="*" element={<Notfound />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <article>
+        <h1>I'm Simon!</h1>
+        <p>Aspiring Web Developer</p>
+      </article>
+    </div>
+  );
 }
 
 export default App;
