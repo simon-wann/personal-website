0c4c7cbf71963df494ae8092702a9150c9d9def2
Simon Wan <simonwan@simons-mbp-2.mynetworksettings.com>
2023-06-19 12:12:57 -0400

    Updated Shared Code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
+import Header from './components/header/Header.jsx'
+import Nav from './components/nav/Nav.jsx'
+import About from './components/about/About.jsx'
+import Experience from './components/experience/Experience.jsx'
+import Portfolio from './components/portfolio/Portfolio.jsx'
+import Contact from './components/contact/Contact.jsx'
+import Footer from './components/footer/Footer.jsx'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <article>
-        <h1>I'm Simon!</h1>
-        <p>Aspiring Web Developer</p>
-      </article>
-    </div>
-  );
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  )
 }
+
 
 export default App;
---
+++ src/components/header/Buttons.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Resume from './'
+
+const Buttons = () => {
+  return (
+    <div classNAme='Buttons'>
+        <a href="">Download Resume</a>
+        <a href="#contact">Contact Me</a>
+    </div>
+  )
+}
+
+export default Buttons
