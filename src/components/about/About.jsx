cfd4b192db003111871300fe64afba2ce4afa54a
Simon Wan <simonwan@Simons-MacBook-Pro-2.local>
2023-06-20 16:25:27 -0400

    Deployed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './about.css'
+import Pfp from '../../assets/Profile Picture.jpg'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+          <h5>Get to know me</h5>
+          <h2>About</h2>
+
+          <div className="container about__container">
+            <div className="about__me">
+              <div className="about__me-image"></div>
+                <img src={Pfp} alt="About Picture" /> 
+            </div>
+
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Contact Me</a>
+          </div>
+
+    </section>
+
   )
 }
 
--- src/components/header/Buttons.jsx
+++ src/components/header/Buttons.jsx
@@ -4,8 +4,8 @@ import Resume from '../../assets/Simon-Wan-Resume.pdf'
 const Buttons = () => {
   return (
     <div className='buttons'>
-        <a href={Resume} download className='btn'>Download Resume</a>
-        <a href="#contact" className='btn btn-primary'>Contact Me</a>
+        <a href={Resume} download className='btn'>Resume</a>
+        <a href="#contact" className='btn btn-primary'>Contact</a>
     </div>
   )
 }
