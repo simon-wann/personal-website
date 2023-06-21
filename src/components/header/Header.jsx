d0b5c248d944ea83547dd16073bbab34662e209e
Simon Wan <simonwan@Simons-MacBook-Pro-2.local>
2023-06-21 09:21:24 -0400

    Deployed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello, I'm</h5>
+        <h5>Hi, my name is</h5>
         <h1>Simon Wan</h1>
         <h5 className='text-light'>Aspiring Web Developer</h5>
         <Buttons />
