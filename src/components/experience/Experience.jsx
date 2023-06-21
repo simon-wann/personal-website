218893f0ff40c34462973676df51cd26e57d16ce
simon-wann <simon460w@gmail.com>
2023-06-21 16:28:17 -0400

    Deployed

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
+import NYPlogo from '../../assets/NYP logo.png'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h2>Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__1">
+          <img src={NYPlogo} />
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+            </article>
+          </div>
+
+        </div>
+
+        <div className="experience__2">
+
+        </div>
+      </div>
+
+    </section>
   )
 }
 
