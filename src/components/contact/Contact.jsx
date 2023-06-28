0765087195399660f56519575938361fd80c2bcf
simon-wann <simon460w@gmail.com>
2023-06-28 14:20:02 -0400

    EmailJS

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {BsChatText} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_01oldjf', 'template_30lfyhg', form.current, 'gxycEc9KYstn-t_-4')
+     
+    e.target.reset()
+  };
+
   return (
     <section id='contact'>
 
@@ -32,7 +44,7 @@ const Contact = () => {
         
 
 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
